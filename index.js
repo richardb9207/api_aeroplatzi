d451daaee26380f2ae954ce02fab273715d93a3e
Ricardo Beltran <richardb9207@gmail.com>
2019-11-14 19:52:30 -0500

    CORS

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const { config } = require('./config/index');
 const ticketsApi = require('./routes/tickets.js');
 
 app.use(express.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-with, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  next();
+});
 
 ticketsApi(app);
 
